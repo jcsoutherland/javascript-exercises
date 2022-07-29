301a4b2ac4ae5d69e2552a33e45c633e29ddeee9
jcsoutherland <jcsoutherland@ksu.edu>
2022-07-28 19:10:36 -0500

    finished getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function (array) {
+  let result = []
+  array.forEach((book) => {
+    result.push(book.title)
+  })
+  return result
+}
 
 // Do not edit below this line
-module.exports = getTheTitles;
+module.exports = getTheTitles
