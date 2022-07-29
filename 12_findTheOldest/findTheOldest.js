bbefcde2f7020223ec7df6cb9080465a2898d7f8
jcsoutherland <jcsoutherland@ksu.edu>
2022-07-28 19:45:51 -0500

    finished findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function (array) {
+  let currentOldest = array[0]
+  let ageArray = []
+  //let ageArray = [array[0]]
+  //console.log(currentOldest.yearOfDeath)
+  array.forEach((person) => {
+    if (!person.yearOfDeath) {
+      ageArray.push({
+        index: array.indexOf(person),
+        age: 2022 - person.yearOfBirth,
+      })
+    } else {
+      ageArray.push({
+        index: array.indexOf(person),
+        age: person.yearOfDeath - person.yearOfBirth,
+      })
+    }
+  })
+  ageArray.sort(function (a, b) {
+    return a.age - b.age
+  })
+  console.log(ageArray)
+  const oldestID = ageArray[ageArray.length - 1].index
+  return array[oldestID]
+}
 
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = findTheOldest
