58eb141f5696651626c13b4b3cc2db3e08bcf2a1
jcsoutherland <jcsoutherland@ksu.edu>
2022-07-28 19:08:12 -0500

    Finished 10_fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
-};
+const palindromes = function (inputString) {
+  const str = inputString.replace(/[^a-zA-z]/g, '').toLowerCase()
+  console.log(str)
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[str.length - 1 - i] && i <= str.length - 1 - i) {
+      return false
+    }
+  }
+  return true
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (num) {
+  if (+num < 0) {
+    return 'OOPS'
+  }
+  let result = [0, 1]
+  for (let i = 2; i <= +num; i++) {
+    result.push(result[i - 1] + result[i - 2])
+  }
+  return result[+num]
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
